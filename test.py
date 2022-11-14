767cb398f17bd6a39bfda50908ec7ff009ad21ae
BuildTools <unconfigured@null.spigotmc.org>
2022-11-13 21:52:15 -0500

    Added more checks, and better word suggestions

--- test.py
+++ test.py
@@ -78,12 +78,22 @@ def wordFilter(wordleList, letters, n):
             #print(i)
             for l in range(len(i)): #step through letters of each word
                 for a in range(len(letters)): #compare letters in word to letters given
+                    #if i == "array":
+                        #print(str(i[l]) + "::" + str(letters[a]) + str(l) + ":" + str(a))
                     if str(i[l]) == str(letters[a]) and l != a:
                         letterC+=1
                         #break
+                        #if i == "array":
+                            #print(str(letterC) + " = " + str(letterCount))
                     if letterC == letterCount:
+                        
                         addWord=1
+                    if str(i[l]) == str(letters[a]) and l == a:
+                        addWord=0
+                        letterC-=1
                     a+=1
+                    #if i == "array":
+                        #print(addWord)
                 #if addWord==1:
                     #print("Word Added")
                     #break
@@ -92,7 +102,28 @@ def wordFilter(wordleList, letters, n):
                 l+=1
             if addWord == 1:
                 filteredList.append(i)
-
+                for x in letters:
+                    if x != ' ':
+                        if x not in i:
+                            filteredList.remove(i)
+                            break
+                
+    if n == 4:
+        #print("green letters:" + str(letters))
+        for i in wordleList: #step through words
+            addWord=True
+            #print(i)
+            for l in i: #step through letters of each word
+                for a in letters: #compare letters in word to letters given
+                    #print(l + " : " + a)
+                    if l == a:
+                        #print("if triggered")
+                        addWord=False
+                        break
+                if addWord==False:
+                    break
+            if addWord == True:
+                filteredList.append(i)
     return filteredList
 
 def inputCorrection(UserIn, Guess):
@@ -107,14 +138,42 @@ def inputCorrection(UserIn, Guess):
                 #print("a= " + str(a))
                 if i == Guess[a]:
                     Correction[a] = i
-    return Correction
+        return Correction
+    else:
+        return UserIn
         
 
 wordle = True
-rnd=1
+rnd=0
 wordleList = WL
+wordSugL = WL
+correctLetters = None
+
 while wordle == True:
-    print("I suggest you try: " + str(wordSuggest.wordSuggest(wordleList)))
+    SW = wordSuggest.wordSuggest(wordleList, wordSugL, correctLetters)
+    if len(SW) == 3:
+        a = 1
+        narrowIn = []
+        for w in SW:
+            b = 0
+            for l in w:
+                #print(l + "=" + SW[a][b])
+                if l != SW[a][b] and a == 1:
+                    narrowIn.append(l)
+                elif l != SW[a][b] and a == 2:
+                    narrowIn.append(l)
+                    narrowIn.append(SW[a][b])
+                b+=1
+            a+=1
+            if a == 3:
+                print(narrowIn)
+                SW = []
+                for w in WL:
+                    if narrowIn[0] in w and narrowIn[1] in w and narrowIn[2] in w:
+                        SW.append(w)
+                break
+
+    print("I suggest you try: " + str(SW))
     print("Did you guess the word right? (y/n)")
     gameOn = input()
     if gameOn == 'y':
@@ -130,15 +189,10 @@ while wordle == True:
         #([*greyLetters])
         #print(greyLetters)
         wordleList = wordFilter(wordleList,greyLetters,1)
-        #print(wordleList)
-        
-    print("Which letters were green? (Type \"none\" if none)")
-    correctLetters=input()
-    if correctLetters!="none":
-        correctLetters=inputCorrection(correctLetters,guess)
-        #([*correctLetters])
-        #print(correctLetters)
-        wordleList = wordFilter(wordleList,correctLetters,2)
+        if rnd == 0:
+            wordSugL = wordleList
+        else:
+            wordSugL = wordFilter(wordSugL, greyLetters, 1)
         #print(wordleList)
         
     print("Which letters are yellow? (Type \"none\" if none)")
@@ -147,7 +201,24 @@ while wordle == True:
         yellowLetters = inputCorrection(yellowLetters,guess)
         #print(yellowLetters)
         wordleList = wordFilter(wordleList,yellowLetters,3)
+        if rnd == 0:
+            wordSugL = wordleList
+        else:
+            wordSugL = wordFilter(wordSugL, yellowLetters, 3)
         #print(wordleList)
+
+    print("Which letters were green? (Type \"none\" if none)")
+    correctLetters=input()
+    if correctLetters!="none":
+        correctLetters=inputCorrection(correctLetters,guess)
+        #([*correctLetters])
+        #print(correctLetters)
+        wordleList = wordFilter(wordleList,correctLetters,2)
+        wordSugL = wordFilter(wordSugL,correctLetters,1)
+        #wordSugL = wordFilter(wordSugL,correctLetters,4)
+        #print(wordleList)
+        #print("*-*-*")
+        #print(wordSugL)
 
     rnd+=1
     if rnd==6:
--- wordSuggest.py
+++ wordSuggest.py
@@ -29,11 +29,14 @@ def letterCount(WL, LL):
         sortRes[w]=res[w]
     return sortRes
 
-def wordSuggest(WSL):
+def wordSuggest(WSL, BGWSL, CL):
     LC = letterCount(WSL,LL)
-    #print(LC)
+    LC2 = letterCount(BGWSL,LL)
+    print(LC)
+    print(LC2)
     x = list(LC.keys())
     #print(x)
+    y = list(LC2.keys())
     count=0
     L=[0]*len(WSL)
     for i in WSL: #word
@@ -59,6 +62,108 @@ def wordSuggest(WSL):
         sortRes[w]=res[w]
     #print(sortRes)
     j = list(sortRes.keys())
+
+    count=0
+    L=[0]*len(BGWSL)
+    for i in BGWSL: #word
+        #print("***********")
+        #print(i)
+        
+        for l in i: #letter
+            for a in range(0,5):
+                #print("*****")
+                #print(y[a])
+                if l == y[a]:
+                    L[count]=L[count]+1
+        count+=1
+    #print(L)
+    res=dict(zip(BGWSL,L))
+    #print(res)
+    sortRes={}
+    sorted_keys=sorted(res, key=res.get, reverse=True)
+
+    for w in sorted_keys:
+        sortRes[w]=res[w]
+    #print(sortRes)
+    k = list(sortRes.keys())
+    #print("*******")
+    #print(j)
+    #print(k)
+
+    if CL == None:
+        if len(k) >= 3:
+            wordSug=['']*5
+            c=0
+            for w in range(0,len(k)): #word
+                #print("*****")
+                #print(k[w])
+                count=0
+                kw=k[w]
+                for l in range(0,5): #letter in word^
+                    #print("----")
+                    #print(kw[l])
+                    for n in range(0,4):
+                        #print(kw[n])
+                        if kw[l] == kw[n+1]:
+                            count+=1
+                        #print(count)
+                if count == 4:
+                    #print(k[w])
+                    wordSug[c] = k[w]
+                    c+=1
+                    if c == 5:
+                        break
+        print("slakhgaslgh" + str(wordSug))
+        return wordSug[0:5]
+
+    print(len(j))
+    print(len(k))
+    if len(j) >= 3 and len(k) > 1:
+        wordSug=['']*5
+        c=0
+        for w in range(0,len(k)): #word
+            #print("*****")
+            #print(k[w])
+            count=0
+            kw=k[w]
+            for l in range(0,5): #letter in word^
+                #print("----")
+                #print(kw[l])
+                for n in range(0,4):
+                    #print(kw[n])
+                    if kw[l] == kw[n+1]:
+                        count+=1
+                    #print(count)
+            if count == 4:
+                #print(k[w])
+                wordSug[c] = k[w]
+                c+=1
+                if c == 5:
+                    break
+        print("slakhgaslgh" + str(wordSug))
+        if wordSug[0] == '':
+            c=0
+            for w in range(0,len(k)): #word
+                #print("*****")
+                #print(k[w])
+                count=0
+                kw=k[w]
+                for l in range(0,5): #letter in word^
+                    #print("----")
+                    #print(kw[l])
+                    for n in range(0,4):
+                        #print(kw[n])
+                        if kw[l] == kw[n+1]:
+                            count+=1
+                        #print(count)
+                if count == 5:
+                    #print(k[w])
+                    wordSug[c] = k[w]
+                    c+=1
+                    if c == 5:
+                        break
+        print("slakhgaslgh" + str(wordSug))
+        return wordSug[0:5]
 
     if len(j) >= 3:
         wordSug=['']*3
